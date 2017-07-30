0b9d4442f2939fc27222b1922a04148b3974d0f1
Hiroyuki Sano <sh19910711@gmail.com>
2017-07-30 09:00:09 +0000

    setup travis-ci

--- setup.bash
+++ setup.bash
@@ -21,6 +21,6 @@ insert ~/.bashrc source ~/.bash.d/src.bash
 
 # ansible
 sudo apt-get install -y software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
+sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y ansible
