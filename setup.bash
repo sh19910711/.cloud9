e19a8211a1191ac7b067babbbe10a4eb3e2d647c
Hiroyuki Sano <sh19910711@gmail.com>
2018-06-17 10:54:24 +0000

    add t2.small instance

--- setup.bash
+++ setup.bash
@@ -6,4 +6,13 @@ Host gpu
   Hostname gpu.cloud9
   User ec2-user
   IdentityFile ~/.ssh/id_gateway
+
+Host t2.small
+  Hostname t2.small.cloud9
+  User ec2-user
+  IdentityFile ~/.ssh/id_gateway
 EOF
+
+sudo iptables -A FORWARD -i eth0 -o eth0 -s 10.0.1.0/24 -j ACCEPT
+sudo iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
+sudo service iptables save
