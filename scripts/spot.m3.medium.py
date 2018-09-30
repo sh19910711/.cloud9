3bfe02c2883adb09987a8260313368bf8337960d
Hiroyuki Sano <sh19910711@gmail.com>
2018-09-30 11:33:07 +0000

    vpc: add subnet

--- scripts/spot.m3.medium.py
+++ scripts/spot.m3.medium.py
@@ -2,10 +2,10 @@ import boto3
 
 ec2 = boto3.client('ec2', region_name='us-east-1')
 res = ec2.request_spot_instances(
-        SpotPrice='0.0318',
+        SpotPrice='0.0067',
         InstanceCount=1,
         LaunchSpecification={
-            'InstanceType': 'm3.large',
+            'InstanceType': 'm3.medium',
             'ImageId': 'ami-cfe4b2b0', # Amazon Linux AMI 2018.03.0 (HVM), SSD Volume Type
             'KeyName': 'gateway',
             'SubnetId': 'subnet-54c51908',
