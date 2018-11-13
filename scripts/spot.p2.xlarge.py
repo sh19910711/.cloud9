7a8c9729dbc40f4e9c3b68d531958705629a0fb6
Hiroyuki Sano <sh19910711@gmail.com>
2018-11-13 21:15:35 +0000

    add spotreq

--- scripts/spot.p2.xlarge.py
+++ scripts/spot.p2.xlarge.py
@@ -2,11 +2,11 @@ import boto3
 
 ec2 = boto3.client('ec2', region_name='us-east-1')
 res = ec2.request_spot_instances(
-        SpotPrice='0.01',
+        SpotPrice='0.3',
         InstanceCount=1,
         LaunchSpecification={
-            'InstanceType': 'm3.medium',
-            'ImageId': 'ami-cfe4b2b0', # Amazon Linux AMI 2018.03.0 (HVM), SSD Volume Type
+            'InstanceType': 'p2.xlarge',
+            'ImageId': 'ami-0d3348bce8bdde5a6',
             'KeyName': 'gateway',
             'SubnetId': 'subnet-54c51908',
             'SecurityGroupIds': [
